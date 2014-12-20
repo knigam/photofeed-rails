3fab5cc21c6e96aa0f0a2b88ae16cf6b7fa87495
Kushal <kushal.nigam1@gmail.com>
2014-12-20 17:32:29 +0530

    first commit, created rails app that lets you upload an image and text to create a picture

---
+++ app/models/picture.rb
@@ -0,0 +1,10 @@
+class Picture < ActiveRecord::Base
+    has_attached_file :image,
+                      :styles => {
+                        :thumb => "75x75>"
+                        :small => "150x150>"
+                      }
+    validates_attachment_presence :image
+    validates_attachment_content_type :image, :content_type => /\Aimage/
+#   validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
+end
---
+++ app/views/pictures/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @picture, :id, :text, :created_at, :updated_at
