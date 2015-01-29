2ce4e8256dc41630e3041d27272ba9556e63f2fc
knigam <kushal.nigam1@gmail.com>
2015-01-29 00:56:39 -0500

    made some fixes for album jsons

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -10,7 +10,10 @@ class AlbumsController < ApplicationController
   # GET /albums/1
   # GET /albums/1.json
   def show
-      redirect_to album_pictures_path(:album_id => params[:id])
+      respond_to do |format|
+          format.html { redirect_to album_pictures_path(:album_id => params[:id]) }
+          format.json {redirect_to album_pictures_path(:album_id => params[:id], format:     :json) }
+      end
   end
 
   # GET /albums/new
--- app/views/pictures/index.json.jbuilder
+++ app/views/pictures/index.json.jbuilder
@@ -2,5 +2,5 @@ json.array!(@pictures) do |picture|
   json.extract! picture, :id, :text
   json.original_url picture.image.url
   json.thumb_url picture.image.url(:thumb)
-  json.url picture_url(picture, format: :json)
+  json.url album_picture_url(:id => picture, format: :json)
 end
